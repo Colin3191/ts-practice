14a7e6e2a08dd5b4d3c9c63ae353e5a7628f22a9
wangchaomin <319151881@qq.com>
2022-08-26 16:00:54 +0800

    feat: 🎸 BuildArray

---
+++ BuildArray.ts
@@ -0,0 +1,9 @@
+export type BuildArray<
+  Length extends number,
+  Item extends unknown,
+  Arr extends unknown[] = []
+> = Arr["length"] extends Length
+  ? Arr
+  : BuildArray<Length, Item, [...Arr, Item]>;
+
+type BuildArrayRes = BuildArray<5, 1>;

28aa52ce5e785d009a6661f2843ef67d563bd93a
wangchaomin <319151881@qq.com>
2022-08-26 16:28:22 +0800

    refactor: 💡 subtract

--- src/Subtract.ts
+++ src/Subtract.ts
@@ -3,7 +3,7 @@ import { BuildArray } from "./BuildArray";
 type Subtract<
   Num1 extends number,
   Num2 extends number
-> = BuildArray<Num1> extends [...arr1: BuildArray<Num2>, ...arr2: infer Rest]
+> = BuildArray<Num1> extends [...BuildArray<Num2>, ...infer Rest]
   ? Rest["length"]
   : never;
 
